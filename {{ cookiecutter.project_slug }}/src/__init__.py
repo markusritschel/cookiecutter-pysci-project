3572ff0a0c93f784bd2e1bab075c8d30ec562afa
markusritschel <github@markusritschel.de>
2021-06-11 19:43:21 +0200

    Add src directory with __init__.py

---
+++ {{ cookiecutter.project_slug }}/src/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+# Make some of the basic directories globally available in your environment
+base_dir = Path(__file__).resolve().parents[1]
+data_dir = base_dir / 'data'
+log_dir  = base_dir / 'logs'
+plot_dir = base_dir / 'reports/figures'
