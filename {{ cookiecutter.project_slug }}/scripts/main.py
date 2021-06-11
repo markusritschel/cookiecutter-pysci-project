4f448a8ce75bb779742dd13d24d104bf008e9cd2
markusritschel <github@markusritschel.de>
2021-06-11 20:41:02 +0200

    Add scripts directory

---
+++ {{ cookiecutter.project_slug }}/scripts/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding utf-8 -*-
+#
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: {{ cookiecutter.full_name }}
+# eMail:  {{ cookiecutter.email }}
+# Date:   {% now 'local', '%Y-%m-%d' %}
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+from __future__ import absolute_import, division, print_function, with_statement
+
+from src import *
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
