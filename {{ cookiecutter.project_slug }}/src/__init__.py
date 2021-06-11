4fc73b335bc550dd8d49a1c31893bb2d3beaed7d
markusritschel <github@markusritschel.de>
2021-06-11 20:58:30 +0200

    Find and load .env automaticall in src/__init__.py

--- {{ cookiecutter.project_slug }}/src/__init__.py
+++ {{ cookiecutter.project_slug }}/src/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
 
 # Make some of the basic directories globally available in your environment
 base_dir = Path(__file__).resolve().parents[1]
@@ -6,3 +7,8 @@ data_dir = base_dir / 'data'
 log_dir  = base_dir / 'logs'
 plot_dir = base_dir / 'reports/figures'
 jupyter_startup_script = base_dir / 'notebooks/jupyter_startup.ipy'
+
+# find .env automagically by walking up directories until it's found
+dotenv_path = find_dotenv()
+# load up the entries as environment variables
+load_dotenv(dotenv_path)
