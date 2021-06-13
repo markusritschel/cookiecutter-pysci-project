31234f48765f00a31c61f03fbdb078892f7d96c6
markusritschel <github@markusritschel.de>
2021-06-14 01:40:25 +0200

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,6 @@ import subprocess
 
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
-subprocess.call(['git', 'commit', '-m', 'Set up new project from cookiecutter template'])
+subprocess.call(['git', 'commit', '-m', 'Set up new project from cookiecutter template https://github.com/markusritschel/cookiecutter-pysci-project'])
 print("If you want to add a remote repository, run `git remote add origin https://github.com/"
       "{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}` (maybe modify the address)")
