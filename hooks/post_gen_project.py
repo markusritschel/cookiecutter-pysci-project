2e57a8d4ce391704a7344349f98d76bea7cda1c4
markusritschel <git@markusritschel.de>
2022-04-09 23:48:01 +0000

    By default rename master branch to "main"

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,7 @@
 import subprocess
 
 subprocess.call(['git', 'init'])
+subprocess.call(['git', 'branch', '-m', 'main'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Set up new project from cookiecutter template https://github.com/markusritschel/cookiecutter-pysci-project'])
 print("If you want to add a remote repository, run `git remote add origin https://github.com/"
