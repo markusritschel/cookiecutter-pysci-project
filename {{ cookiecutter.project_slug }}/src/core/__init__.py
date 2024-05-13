a223b2fa542053cf9522024da249a309f386c04b
markusritschel <git@markusritschel.de>
2024-05-13 16:21:47 +0200

    Make core a package

---
+++ {{ cookiecutter.project_slug }}/src/core/__init__.py
@@ -0,0 +1,11 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: Markus Ritschel
+# eMail:  git@markusritschel.de
+# Date:   2024-05-13
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import logging
+
+
+log = logging.getLogger(__name__)
+
