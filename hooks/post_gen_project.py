26a2bfc0fcd4e4ba450bc1b4cb8be391990762a7
markusritschel <git@markusritschel.de>
2024-05-28 12:41:40 +0200

    Add colored output to post-hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,25 @@
 """This script executes some commands after the project has been successfully set up from the cookiecutter template"""
 import subprocess
+import os
+from colorama import Fore, Back, Style
 
 print("Initialize Git repository and make a first commit")
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'branch', '-m', 'main'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Set up new project from cookiecutter template https://github.com/markusritschel/cookiecutter-pysci-project'])
-print("If you want to add a remote repository, run `git remote add origin https://github.com/"
-      "{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}` (maybe modify the address)")
 
-print("The project is all set. It is recommended to create a conda environment. You can do this "
-      "either manually or by running the respective command from the Makefile (hint: run `make` to get help).")
+print(Fore.GREEN)
+print("="*100)
+print("")
+
+print(f"  ✅ The project is all set now. Please navigate now to the new directory {os.getcwd()}/. ")
+
+print("  👉 If you want to add a remote git repository, run \n" + Fore.BLUE +
+      "       git remote add origin https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }} \n" + Fore.GREEN +
+      "     from the new directory.")
+
+print("  👉 It is furthermore recommended to create a conda environment (hint: run `make` to get help).")
       
+print("")
+print("="*100)
