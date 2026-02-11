df7a3f59e74d533c1d7921e4cca462b2147d46a9
Markus Ritschel <git@markusritschel.de>
2026-02-11 13:31:06 +0100

    Add condition to skip logger for FileSourceType in Sphinx config

--- {{ cookiecutter.project_slug }}/docs/_sphinx_config.py
+++ {{ cookiecutter.project_slug }}/docs/_sphinx_config.py
@@ -1,6 +1,7 @@
 def skip_logger(app, what, name, obj, skip, options):
     skip_conditions = [
         obj.name.startswith('log'),
+        name.endswith("FileSourceType"),  # Hack to prevent error from sphinx.ext.viewcode, which currently doesn't work with enum classes
         'test' in obj.name,
     ]
     for condition in skip_conditions:
