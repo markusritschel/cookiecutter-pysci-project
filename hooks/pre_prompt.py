30889d95e70c1dc57fed4de1048a85f3c0608a30
Markus Ritschel <git@markusritschel.de>
2026-02-13 13:22:11 +0100

    Add pre_prompt hook

---
+++ hooks/pre_prompt.py
@@ -0,0 +1,8 @@
+msg = """⚠️ Important: You will now be asked to fill in some information about your project.
+
+When asked for the project name, you may use a normal name with spaces, e.g. "My Cool Project". 
+The project slug (which is used for the directory name and the package name) will be generated automatically from the project name by replacing spaces with dashes and converting to lowercase, e.g. "my-cool-project". 
+For the python package name, remaining dashes will be replaced with underscores, e.g. "my_cool_project", to ensure proper naming.
+Of course, you can also directly specify the project slug and package name if you want to use a different naming convention.
+"""
+print(msg)
