ce381f4fc2583eb1fd84e5c1027538018b21f160
Markus Ritschel <git@markusritschel.de>
2026-03-23 15:07:53 +0100

    Update cookiecutter hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,46 +3,102 @@ import os
 import shutil
 import subprocess
 
-if not {{ cookiecutter.is_research_project }}:
-    REMOVE_PATHS = [
-        "data/",
-        "notebooks/",
-        "references/",
-        "reports/",
-        "scripts/",
-    ]
-    for path in REMOVE_PATHS:
+
+# /// 1. Remove redundant paths ///
+
+def remove_recursively(paths):
+    for path in paths:
         if path and os.path.exists(path):
             if os.path.isdir(path):
                 shutil.rmtree(path)
             else:
                 os.unlink(path)
 
+sphinx_files = [
+    'docs/conf.py',
+    'docs/_config/',
+    'docs/changelog.md',
+    
+]
+zensical_files = [
+    'zensical.toml'
+]
+myst_files = [
+    'docs/myst.yml'
+]
+research_project_files = [
+    'data/',
+    'notebooks/',
+    'references/',
+    'reports/',
+    'scripts/'
+]
 
-print("Initialize Git repository and make a first commit")
-subprocess.call(['git', 'init'])
-subprocess.call(['git', 'branch', '-m', 'main'])
-subprocess.call(['git', 'add', '*'])
-subprocess.call(['git', 'commit', '-m', 'Set up new project from cookiecutter template https://github.com/markusritschel/cookiecutter-pysci-project'])
+if not {{ cookiecutter.is_research_project }}:
+    remove_recursively(research_project_files)
+
+if "{{ cookiecutter.docs_engine|lower }}" == "sphinx":
+    remove_recursively(zensical_files)
+    remove_recursively(myst_files)
+elif "{{ cookiecutter.docs_engine|lower }}" == "zensical":
+    remove_recursively(sphinx_files)
+    remove_recursively(myst_files)
+
+elif "{{ cookiecutter.docs_engine|lower }}" == "myst":
+    remove_recursively(sphinx_files)
+    remove_recursively(zensical_files)
+
+
+# /// 2. Initialize Git repository ///
+
+if not os.path.exists(".git"):
+     print("Initialize Git repository and make a first commit")
+     subprocess.run(["git", "init"], check=True)
+     subprocess.run(['git', 'branch', '-m', 'main'], check=True)
+     subprocess.run(["git", "add", "."], check=True)
+     subprocess.run(["git", "commit", "-m", "Set up new project from cookiecutter template https://github.com/markusritschel/cookiecutter-pysci-project"], check=True)
+
+
+# /// 3. Print message ///
 
 GREEN = "\033[92m"
 BLUE = "\033[94m"
 RESET = "\033[0m"
 
+DEFAULT = GREEN
+CODE = BLUE
+
+msg = f"""
+    🎉 Project setup complete! How to get started:
+    ----------------------------------------------
+
+    1. Change directory into your project (if you aren't already):
+         {CODE}cd {os.getcwd()}/ {DEFAULT}
+
+    2. Activate your virtual environment (see the README.md for more details):
+         On Linux/macOS: {CODE}source .venv/bin/activate {DEFAULT}
+         On Windows:    {CODE}.\.venv\Scripts\activate {DEFAULT}
+
+    3. Add a remote git repository (optional): {CODE}
+         {CODE}git remote add origin https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }} {DEFAULT}
+
+    4. To add packages, use:
+         {CODE}uv add <package_name> {DEFAULT}
+
+    5. To run scripts, use:
+         {CODE}uv run python <script.py> {DEFAULT}
+
+    6. To build documentation:
+         {CODE}just docs {DEFAULT}
+
+
+    Happy coding! 🚀
+"""
+
 print(GREEN)
 print("="*100)
-print("")
-
-print(f"  ✅ The project is all set now.")
-print(f"     Please navigate now to the new directory {os.getcwd()}/. ")
-
-print(f"  👉 If you want to add a remote git repository, run \n" + 
-      f"       {BLUE}git remote add origin https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }} \n" + GREEN +
-      f"     from the new directory.")
-
-print("  👉 It is furthermore recommended to work in a virtual environment (hint: run `make` to get help).")
-print("  ℹ️ For further information on how to use the project, please check the README.md file.")
-      
+print(msg)
 print("")
 print("="*100)
 print(RESET)
+
--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -1,10 +1,22 @@
-msg = """⚠️ Important: You will now be asked to fill in some information about your project.
+GREEN = "\033[92m"
+BLUE  = "\033[94m"
+RESET = "\033[0m"
 
-When asked for the project name, you may use a normal name with spaces, e.g. "My Cool Project". 
-The project slug (which is used for the directory name and the package name) will be generated automatically from the project name by replacing spaces with dashes and converting to lowercase, e.g. "my-cool-project". 
-For the python package name, remaining dashes will be replaced with underscores, e.g. "my_cool_project", to ensure proper naming.
-Of course, you can also directly specify the project slug and package name if you want to use a different naming convention.
+msg = f"""
+{GREEN}☝🏼 Important: You will now be asked to fill in some information about your project.
+{RESET}
+When asked for the project name [4], you may use a normal name with spaces, e.g. "My new Project". 
+The project slug [5] (which is used for the directory name and the package name) will be generated 
+automatically from the project name by replacing spaces with dashes and converting to lowercase, 
+e.g. "my-new-project". For the python package name [6], remaining dashes will be replaced with 
+underscores ("my_new_project") to ensure proper naming.
+Of course, you can also directly specify the project slug and package name if you want to use a 
+different naming convention.
 
-If you answer "yes" to the question "Is this a research project?", some additional directories will be created for you (e.g. data, notebooks, references, results, src). If you answer "no", these directories will not be created.
+{GREEN}👉🏼 Research Projects: {RESET}If you answer "yes" to the question "Is this a research project?" [7], some 
+additional directories will be created for you (e.g. data, notebooks, references, results, src). 
+If you answer "no", these directories will not be created.
+
+Let's go 😃
 """
 print(msg)
