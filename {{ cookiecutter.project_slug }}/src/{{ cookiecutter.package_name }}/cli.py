154b877cc4d2d7ff43a374c4b73bf378d1a6cd89
Markus Ritschel <git@markusritschel.de>
2026-03-23 16:24:53 +0100

    Add welcome message to CLI

--- {{ cookiecutter.project_slug }}/src/{{ cookiecutter.package_name }}/cli.py
+++ {{ cookiecutter.project_slug }}/src/{{ cookiecutter.package_name }}/cli.py
@@ -10,6 +10,18 @@ console = Console()
 @app.command()
 def main():
     """CLI script for {{ cookiecutter.package_name }}."""
+    console.print(r"""
+  $$\      $$\           $$\                                             
+  $$ | $\  $$ |          $$ |                                            
+  $$ |$$$\ $$ | $$$$$$\  $$ | $$$$$$$\  $$$$$$\  $$$$$$\$$$$\   $$$$$$\  
+  $$ $$ $$\$$ |$$  __$$\ $$ |$$  _____|$$  __$$\ $$  _$$  _$$\ $$  __$$\ 
+  $$$$  _$$$$ |$$$$$$$$ |$$ |$$ /      $$ /  $$ |$$ / $$ / $$ |$$$$$$$$ |
+  $$$  / \$$$ |$$   ____|$$ |$$ |      $$ |  $$ |$$ | $$ | $$ |$$   ____|
+  $$  /   \$$ |\$$$$$$$\ $$ |\$$$$$$$\ \$$$$$$  |$$ | $$ | $$ |\$$$$$$$\ 
+  \__/     \__| \_______|\__| \_______| \______/ \__| \__| \__| \_______|
+                                                                      
+    """, style="green")
+    # https://patorjk.com/software/taag/
     console.print("Replace this message by putting your code into "
                "{{ cookiecutter.package_name }}.cli.main")
     console.print("See Typer documentation at https://typer.tiangolo.com/")
