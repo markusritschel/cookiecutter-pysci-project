ce5a67c9a1bfc0eb10ab0b6200757f0371605b1d
Markus Ritschel <git@markusritschel.de>
2026-05-19 17:28:36 +0200

    fix: Enhance Git initialization process to handle Cruft updates

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 import subprocess
+import tempfile
 
 
 # /// 1. Remove redundant paths ///
@@ -51,14 +52,19 @@ elif "{{ cookiecutter.docs_engine|lower }}" == "myst":
 
 # /// 2. Initialize Git repository ///
 
-if shutil.which("git") and not os.path.exists(".cruft.json"):
-    if not os.path.exists(".git"):
-        print("Initialize Git repository and make a first commit")
-        subprocess.run(["git", "init"], check=True)
-        subprocess.run(['git', 'branch', '-m', 'main'], check=True)
-        subprocess.run(["git", "add", "."], check=True)
-        subprocess.run(["git", "rm", "--cached", "notebooks/*.ipynb", "scripts/*.py", "src/{{cookiecutter.package_name}}/cli.py", "src/{{cookiecutter.package_name}}/submodule.py", "tests/*.py"])
-        subprocess.run(["git", "commit", "-m", "Set up new project from cookiecutter template https://github.com/markusritschel/cookiecutter-pyproject"], check=True)
+is_cruft_update = os.getcwd().startswith(tempfile.gettempdir())
+
+if is_cruft_update:
+    print("Detected Cruft update process. Skipping git initialization and dependency installation.")
+else:
+    if shutil.which("git"):
+        if not os.path.exists(".git"):
+            print("Initialize Git repository and make a first commit")
+            subprocess.run(["git", "init"], check=True)
+            subprocess.run(['git', 'branch', '-m', 'main'], check=True)
+            subprocess.run(["git", "add", "."], check=True)
+            subprocess.run(["git", "rm", "--cached", "notebooks/*.ipynb", "scripts/*.py", "src/{{cookiecutter.package_name}}/cli.py", "src/{{cookiecutter.package_name}}/submodule.py", "tests/*.py"])
+            subprocess.run(["git", "commit", "-m", "Set up new project from cookiecutter template https://github.com/markusritschel/cookiecutter-pyproject"], check=True)
         if shutil.which("uv"):
             print("Install development dependencies and set up pre-commit hooks")
             subprocess.run(["uv", "sync", "--dev"], check=True)
@@ -69,9 +75,9 @@ if shutil.which("git") and not os.path.exists(".cruft.json"):
             print("WARNING: `uv` not found. Run the following manually after installation:")
             print("  uv sync --dev")
             print("  uv run pre-commit install")
-else:
-    print("WARNING: `git` not found. Initialize the repository manually after installation:")
-    print("  git init && git branch -m main && git add . && git commit -m 'Initial commit'")
+    else:
+        print("WARNING: `git` not found. Initialize the repository manually after installation:")
+        print("  git init && git branch -m main && git add . && git commit -m 'Initial commit'")
 
 
 # /// 3. Print message ///
